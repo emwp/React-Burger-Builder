ada667519189737aefcba72e8cb1760aa88f8e1e
Everton Pereira <everton.mwp@gmail.com>
2019-01-29 00:20:42 -0200

    added burger componets css and build controls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+// import Aux from './hoc/Aux';
 
 class App extends Component {
   render() {
