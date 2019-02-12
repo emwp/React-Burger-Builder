e70aa231065cfb456e787d826bed822a928d6f90
Everton Pereira <everton.mwp@gmail.com>
2019-02-11 23:02:49 -0200

    handling form submission

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,22 +1,34 @@
 import React from 'react';
+
 import classes from './Input.module.css';
 
-const Input = props => {
+const input = props => {
   let inputElement = null;
+
   switch (props.elementType) {
     case 'input':
       inputElement = (
-        <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
       );
       break;
     case 'textarea':
       inputElement = (
-        <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} />
+        <textarea
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
       );
       break;
     case 'select':
       inputElement = (
-        <select className={classes.InputElement} value={props.value}>
+        <select className={classes.InputElement} value={props.value} onChange={props.changed}>
           {props.elementConfig.options.map(option => (
             <option key={option.value} value={option.value}>
               {option.displayValue}
@@ -27,7 +39,12 @@ const Input = props => {
       break;
     default:
       inputElement = (
-        <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
       );
   }
 
@@ -39,4 +56,4 @@ const Input = props => {
   );
 };
 
-export default Input;
+export default input;
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+
 import Button from '../../../components/UI/Button/Button';
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
-import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
@@ -14,6 +15,7 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Your Name',
         },
+        value: '',
       },
       street: {
         elementType: 'input',
@@ -21,13 +23,15 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Street',
         },
+        value: '',
       },
       zipCode: {
         elementType: 'input',
         elementConfig: {
           type: 'text',
-          placeholder: 'Zipcode',
+          placeholder: 'ZIP Code',
         },
+        value: '',
       },
       country: {
         elementType: 'input',
@@ -35,15 +39,16 @@ class ContactData extends Component {
           type: 'text',
           placeholder: 'Country',
         },
+        value: '',
       },
       email: {
         elementType: 'input',
         elementConfig: {
           type: 'email',
-          placeholder: 'Your Email',
+          placeholder: 'Your E-Mail',
         },
+        value: '',
       },
-
       deliveryMethod: {
         elementType: 'select',
         elementConfig: {
@@ -52,29 +57,45 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' },
           ],
         },
+        value: '',
       },
     },
-
     loading: false,
   };
 
-  orderHandler = e => {
-    e.preventDefault();
-    console.log(this.props.ingredients);
+  orderHandler = event => {
+    event.preventDefault();
     this.setState({ loading: true });
+    const formData = {};
+    for (let formElementIdentifier in this.state.orderForm) {
+      formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+    }
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
+      orderData: formData,
     };
     axios
       .post('/orders.json', order)
-      .then(res => {
+      .then(response => {
         this.setState({ loading: false });
         this.props.history.push('/');
       })
-      .catch(err => {
+      .catch(error => {
         this.setState({ loading: false });
       });
+  };
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm,
+    };
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier],
+    };
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({ orderForm: updatedOrderForm });
   };
 
   render() {
@@ -86,19 +107,17 @@ class ContactData extends Component {
       });
     }
     let form = (
-      <form>
+      <form onSubmit={this.orderHandler}>
         {formElementsArray.map(formElement => (
           <Input
             key={formElement.id}
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-
-        <Button btnType="Success" clicked={this.orderHandler}>
-          ORDER
-        </Button>
+        <Button btnType="Success">ORDER</Button>
       </form>
     );
     if (this.state.loading) {
@@ -106,7 +125,7 @@ class ContactData extends Component {
     }
     return (
       <div className={classes.ContactData}>
-        <h4>Enter your contact data</h4>
+        <h4>Enter your Contact Data</h4>
         {form}
       </div>
     );
